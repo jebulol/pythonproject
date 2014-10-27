06ed20e6b64873255ca8903fe42eb92db3e32905
jebulol <jebulol@gmail.com>
2014-10-27 13:39:20 +0200

    Submit t3

---
+++ teht1_3.py
@@ -0,0 +1,9 @@
+nimi1 = input("Anna oma nimi")
+x = input("Anna oma ikä")
+ikä1 = int(x)
+nimi2 = input("Anna äidin nimi")
+x = input("Anna äidin ikä")
+ikä2 = int(x)
+ikäero = ikä2 - ikä1
+print(nimi1, "on",ikäero,"v. nuorempi kuin", nimi2, "äitinsä")
+
